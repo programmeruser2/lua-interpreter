25d4b65f854abecfde83af278c6fd921aebb163e
programmeruser2 <69394951+programmeruser2@users.noreply.github.com>
2020-12-28 21:47:03 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,44 @@ const parse = require('./parser');
 const interpret = require('./interpeter');
 
 const fs = require('fs');
-const file = 'source.lua';
-const code = fs.readFileSync(file).toString();
+const readline = require('readline');
+const rl = readine.createInterface({
+  output: process.stdout,
+  input: process.stdin
+});
 
-const tokens = lex(code);
-const trees = parse(tokens);
-const output = interpret(trees);
-console.log('\x1b[32m=> ' + output + '\x1b[0m');
+function run(code) {
+  const tokens = lex(code);
+  const trees = parse(tokens);
+  const output = interpret(trees);
+  console.log('\x1b[32m=> ' + output + '\x1b[0m');
+}
+
+function repl() {
+  rl.question('> ', code => {
+    try {
+      run(code);
+    } catch(err) {
+      console.error(err);
+    }
+    repl();
+  });
+}
+
+let args;
+if (process.stdout[0].indexOf('node') !== -1) {
+  args = process.stdout.slice(2)
+} else {
+  args = process.stdout.slice(1);
+}
+if (args.length <= 0) {
+  console.log('Lua Interpreter REPL');
+  repl();
+} else {
+  const file = args[0];
+  const code = fs.readFileSync(file).toString();
+  run(code);
+}
+
+
+
